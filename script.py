439ea2e76b0fd26898fa0a2f802c892808315257
Vlad Arama <vlad.arama@ericsson.com>
2024-01-17 12:18:46 -0500

    working ip and endpoint anonymization
    This commit does the following:
    - working ip / endpoint (does not anonymize endpoints with numbers).
    - adds command line arguments to specify which type of anonymization is
      wanted.
    - user id anonymization needs fixing (find correct pattern)

--- script.py
+++ script.py
@@ -1,17 +1,25 @@
-import re
-import namesgenerator
 import os
 import random
+import re
+import namesgenerator
+import argparse
 
 base_directory = './logs/seroius03161/archive_log'
 output_directory = './logs/anonymized_logs2'
 lookup_table = {}
 
-endpoint_pattern = r'\/(?![0-9])([^\/\s]+)'
+endpoint_pattern = r'\b[^\/\s]+\/(?![0-9])[^\/\s]+(?:\/(?![0-9])[^\/\s]+)?'
 ip_pattern = r'\b\d{1,3}(?:\.\d{1,3}){2,}\b'
-# user_id_pattern = r'(?<=\s)[a-zA-Z_\-][a-zA-Z0-9_\-]{3,}(?=\s)'
+user_id_pattern = r'- ([a-zA-Z_\-][a-zA-Z0-9_\-]*)\s'
 
 exclude_extensions = ['.gz', '.md5', '.sha1', '.sha256', '.zip']
+
+parser = argparse.ArgumentParser(description="Anonymize log files. Anonymized files and their lookup tables are storedd in the `anonymized-logs` folder")
+parser.add_argument('--ip', action='store_true', help='Only Anonymize IP Addresses')
+parser.add_argument('--endpoint', action='store_true', help='Only Anonymize Endpoints')
+parser.add_argument('--user', action='store_true', help='Only Anonymize User IDs')
+
+args = parser.parse_args()
 
 os.makedirs(output_directory, exist_ok=True)
 
@@ -27,8 +35,12 @@ def anonymize_endpoint(match):
             lookup_table[part] = anonymized_part
 
         anonymized_parts.append(anonymized_part)
-    anonymized_endpoint = '/' + '/'.join(anonymized_parts)
+    if original_endpoint.startswith('/'):
+        anonymized_endpoint = '/' + '/'.join(anonymized_parts)
+    else:
+        anonymized_endpoint = '/'.join(anonymized_parts)
     return anonymized_endpoint
+
 
 def anonymize_ip(match):
     original_ip = match.group(0)
@@ -36,22 +48,26 @@ def anonymize_ip(match):
         anonymized_ip = lookup_table[original_ip]
     else:
         ip_parts = original_ip.split('.')
-        anonymized_ip = ".".join([str(random.randint(0, 255)).zfill(len(part)) for part in ip_parts])
+        anonymized_ip_parts = []
+        for part in ip_parts:
+            anonymized_part = str(random.randint(0, 255))
+            anonymized_ip_parts.append(anonymized_part)
+        anonymized_ip = ".".join(anonymized_ip_parts)
         lookup_table[original_ip] = anonymized_ip
 
     return anonymized_ip
 
 def anonymize_user_id(match):
-    original_user_id = match.group(0)
+    original_user_id = match.group(1)
     if original_user_id in lookup_table:
         anonymized_user_id = lookup_table[original_user_id]
     else:
         anonymized_user_id = namesgenerator.get_random_name()
         lookup_table[original_user_id] = anonymized_user_id
 
-    return anonymized_user_id
+    return '- ' + anonymized_user_id + ' ' 
 
-# Iterate through all files in the base directory
+
 for filename in os.listdir(base_directory):
     if not filename.endswith(tuple(exclude_extensions)):
         input_file_path = os.path.join(base_directory, filename)
@@ -60,9 +76,16 @@ for filename in os.listdir(base_directory):
 
         with open(input_file_path, 'r') as input_file, open(output_file_path, 'w') as output_file:
             for line in input_file:
-                line = re.sub(ip_pattern, anonymize_ip, line)
-                line = re.sub(endpoint_pattern, anonymize_endpoint, line)
-                # line = re.sub(user_id_pattern, anonymize_endpoint, line)
+                if args.ip:
+                    line = re.sub(ip_pattern, anonymize_ip, line)
+                if args.endpoint:
+                    line = re.sub(endpoint_pattern, anonymize_endpoint, line)
+                if args.user:
+                    line = re.sub(user_id_pattern, anonymize_user_id, line)
+                if not (args.ip or args.endpoint or args.user):
+                    line = re.sub(ip_pattern, anonymize_ip, line)
+                    line = re.sub(endpoint_pattern, anonymize_endpoint, line)
+                    line = re.sub(user_id_pattern, anonymize_user_id, line)
                 output_file.write(line)
                 
 
